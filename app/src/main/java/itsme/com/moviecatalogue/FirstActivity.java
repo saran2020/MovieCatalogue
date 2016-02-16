697dca24c963586d6087b75a4ee9ae1a51b876be
saran2020 <saran.sankaran@gmail.com>
2016-02-17 01:38:47 +0530

    New Project
    A Blank new project of movie catalogue

---
+++ app/src/main/java/itsme/com/moviecatalogue/FirstActivity.java
@@ -0,0 +1,13 @@
+package itsme.com.moviecatalogue;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FirstActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first);
+    }
+}
