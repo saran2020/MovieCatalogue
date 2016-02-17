9b04d5b6163dff86052fe25fb516c938b6988693
saran2020 <saran.sankaran@gmail.com>
2016-02-18 04:30:53 +0530

    Get JSON Data from the cloud and stores it locally.

--- app/src/main/java/itsme/com/moviecatalogue/GridViewFragment.java
+++ app/src/main/java/itsme/com/moviecatalogue/GridViewFragment.java
@@ -1,8 +1,10 @@
 package itsme.com.moviecatalogue;
 
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -10,6 +12,22 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.GridView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 /**
  * Created by its me on 17-Feb-16.
@@ -52,12 +70,157 @@ public class GridViewFragment extends Fragment {
 
         GridView gridViewMovie = (GridView) rootView.findViewById(R.id.gridview_movie_list);
         gridViewMovie.setAdapter(new GridViewAdapter(getActivity().getBaseContext()));
+
+        FetchMovieDataTask fetchData = new FetchMovieDataTask();
+        fetchData.execute();
     }
 
-    /*//User declared classes
+    //User declared classes
     private class FetchMovieDataTask extends AsyncTask<Void, Void, Void>{
+        //Constants and Global Variables
+        private final String LOG_TAG = GridViewFragment.class.getSimpleName();
 
+        final String[] TITLE = new String[20];
+        final String[] RELEASE_DATE = new String[20];
+        final String[] OVERVIEW = new String[20];
+        final String[] IMAGE = new String[20];
+        final Float[] RATING = new Float[20];
 
+        //Overrided methods
+        @Override
+        protected Void doInBackground(Void... params) {
 
-    }*/
+            HttpURLConnection URLConnection = null;
+            BufferedReader reader = null;
+            String jsonString = null;
+
+            try {
+                final String BASE_URL = "https://api.themoviedb.org/3/discover/movie?";
+                final String SORT = "sort_by";
+                final String APP_ID = "api_key";
+
+                Uri uri = Uri.parse(BASE_URL).buildUpon().appendQueryParameter(SORT, "popularity.desc")
+                        .appendQueryParameter(APP_ID, BuildConfig.THE_MOVIE_DB_API_KEY)
+                        .build();
+
+                String url = uri.toString();
+                Log.v(LOG_TAG, "URL: " + url);
+
+                URL urlObject = new URL(url);
+
+                URLConnection = (HttpURLConnection) urlObject.openConnection();
+                URLConnection.setRequestMethod("GET");
+                URLConnection.setConnectTimeout(5000);
+                URLConnection.setReadTimeout(5000);
+                URLConnection.connect();
+
+                InputStream inputStream = URLConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if (inputStream == null) {
+                    return null;
+                }
+
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+
+                while ((line = reader.readLine()) != null)
+                    buffer.append(line + "/n");
+
+                if (buffer.length() == 0)
+                    return null;
+
+                jsonString = buffer.toString();
+
+                Log.v(LOG_TAG, "JSONSTring" + jsonString);
+            } catch (IOException e) {
+                Log.e("FetchMovieDataTask", "ERROR" + e.toString());
+            } finally {
+                if (URLConnection != null) {
+                    URLConnection.disconnect();
+                }
+
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (IOException e) {
+                        Log.e("FetchMovieDataTask", "ERROR closing stream" + e.toString());
+                    }
+                }
+            }
+
+            try {
+                formatJsonString(jsonString);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        //User defined methods
+        private void formatJsonString(String jsonString)
+                throws JSONException {
+
+            //Required for formatting
+            final String JSON_RESULT = "results";
+
+            //Required Fields
+            final String JSON_TITLE = "original_title";
+            final String JSON_RELEASE_DATE = "release_date";
+            final String JSON_OVERVIEW = "overview";
+            final String JSON_IMAGE = "poster_path";
+            final String JSON_RATING = "vote_average";
+
+            //Getting a JSON object for the complete String
+            JSONObject movieDetailJson = new JSONObject(jsonString);
+            //Getting a array of 20 movies from the Json
+            JSONArray movieArray = movieDetailJson.getJSONArray(JSON_RESULT);
+
+            for (int pos = 0; pos < movieArray.length(); pos++) {
+                String title;
+                String releaseDate;
+                String overView;
+                String poster;
+                String rating;
+
+                //Get single movie from a array of 20 movies
+                JSONObject movie = movieArray.getJSONObject(pos);
+
+                //Getting data for a single movie
+                title = movie.getString(JSON_TITLE);
+                releaseDate = movie.getString(JSON_RELEASE_DATE);
+                overView = movie.getString(JSON_OVERVIEW);
+                poster = movie.getString(JSON_IMAGE);
+                rating = movie.getString(JSON_RATING);
+
+                updateToArray(pos, title, releaseDate, overView, poster, rating);
+            }
+
+            //Exiting the method after populating the array
+            return;
+        }
+
+        private void updateToArray(Integer pos, String title, String releaseDate
+                , String overView, String poster, String rating) {
+
+            //Getting date from Year-Month-Date format
+            DateFormat format = new SimpleDateFormat("yyyy-MM-dd", Locale.ENGLISH);
+            Date date = null;
+            try {
+                date = format.parse(releaseDate);
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+
+            //Changing date to Date-Month-Year format
+            format = new SimpleDateFormat("dd-MM-yyyy", Locale.ENGLISH);
+
+            TITLE[pos] = title;
+            RELEASE_DATE[pos] = format.format(date);
+            OVERVIEW[pos] = overView;
+            IMAGE[pos] = poster;
+            RATING[pos] = Float.valueOf(rating);
+        }
+
+    }
 }
