ea8b60ffb34e3888e8670b358dae706dab361b30
saran2020 <saran.sankaran@gmail.com>
2016-05-18 03:43:01 +0530

    Fixed the gridViewAdpater to make it working with cursor loader.

--- app/src/main/java/itsme/com/moviecatalogue/GridViewAdapter.java
+++ app/src/main/java/itsme/com/moviecatalogue/GridViewAdapter.java
@@ -1,83 +1,58 @@
 package itsme.com.moviecatalogue;
 
 import android.content.Context;
-import android.util.Log;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.support.v4.widget.CursorAdapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
 import android.widget.ImageView;
-
-import com.squareup.picasso.Picasso;
 
 /**
  * Created by its me on 17-Feb-16.
  */
-public class GridViewAdapter extends BaseAdapter {
+public class GridViewAdapter extends CursorAdapter {
 
     //Constants and Global variables
     private Context mContext;
-    private String[] imageResource;
-    private boolean SHOULD_UPDATE_RESOURCE = false;
-    private Integer[] image ={R.drawable.w185_2};
 
-    //Constructor
-    public GridViewAdapter(Context context, String[] Image) {
-        this.mContext = context;
-        this.imageResource = Image;
-    }
-
-    //OverridedMethods
-    @Override
-    public int getCount() {
-        return imageResource.length;
-        //return 3;
+    public GridViewAdapter(Context context, Cursor c) {
+        super(context, c);
+        mContext = context;
     }
 
     @Override
-    public Object getItem(int position) {
-        return null;
-    }
-
-    @Override
-    public long getItemId(int position) {
-        return 0;
-    }
-
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-
+    public View newView(Context context, Cursor cursor, ViewGroup parent) {
         //getting the layout inflater for inflating the layout.
-        LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(
-                Context.LAYOUT_INFLATER_SERVICE);
-        ImageView imageView;
-
-        if (convertView == null) {
-            //This helps us in inflating the gridview with many imageViews.
-            imageView = (ImageView) inflater.inflate(R.layout.grid_view_item, null);
-        } else {
-            imageView = (ImageView) convertView;
-        }
-
-        //This methods create a string array full of url for the images to be populated.
-        if (!SHOULD_UPDATE_RESOURCE)
-            updateResource();
-
-        Log.v("AdapterClass:",imageResource[position]);
-
-        Picasso.with(mContext).setLoggingEnabled(false);
-        Picasso.with(mContext)
-                .load(imageResource[position])
-                .into(imageView);
-
-        return imageView;
+        View view = LayoutInflater.from(mContext).inflate(R.layout.grid_view_item, null);
+        view.setTag(new ViewHolder(view));
+        return view;
     }
 
-    //User Defined Methods
-    private void updateResource() {
-        for(int i = 0; i < imageResource.length; i++){
-            imageResource[i] = "http://image.tmdb.org/t/p/w185/" + imageResource[i];
+    @Override
+    public void bindView(View view, Context context, Cursor cursor) {
+        ViewHolder viewHolder = (ViewHolder) view.getTag();
+
+        ImageView imageView = viewHolder.poster;
+        Bitmap poster = Utility.getImage(cursor.getBlob(GridViewFragment.PROJ_POSTER));
+        imageView.setImageBitmap(poster);
+
+
+        /*Picasso.with(mContext).setLoggingEnabled(false);
+
+        Picasso.with(mContext)
+                .load(poster)
+                .into(imageView);
+*/
+    }
+
+
+    public static class ViewHolder {
+        public final ImageView poster;
+
+        public ViewHolder(View view) {
+            poster = (ImageView) view.findViewById(R.id.image_view_poster);
         }
-        SHOULD_UPDATE_RESOURCE = true;
     }
 }
